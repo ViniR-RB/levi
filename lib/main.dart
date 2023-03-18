1b74161e575360ac7b056194f0a3764840af1857
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2023-03-18 12:22:43 -0300

    feat: remove dotenv dependencie;

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'app/app_widget.dart';
 
 void main() async {
-  await DotEnv().load(fileName: '.env');
+  // await DotEnv().load(fileName: '.env');
   runApp(const MyApp());
 }
