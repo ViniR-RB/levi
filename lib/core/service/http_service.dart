7fcf28139ab7017a59baffa98bf34479547414ea
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2023-03-18 12:24:49 -0300

    feat: changed http service

--- lib/core/service/http_service.dart
+++ lib/core/service/http_service.dart
@@ -1,17 +1,19 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:captura/core/constants.dart';
 import 'package:http/http.dart' as http;
 
-import '../constants.dart';
+class HttpService {
+  HttpService();
 
-class HttpSerivce {
-  Future<void> uploadAudio(File audioFile) async {
+  Future<String> uploadAudio(File audioFile, String template) async {
+    Uri url = Uri.parse('$baseUrl?gabarito=$template');
     Uint8List audioConverted = await audioFile.readAsBytes();
-    print(audioConverted);
+
     http.MultipartRequest request = http.MultipartRequest(
       'POST',
-      Uri.parse(url.toString()),
+      url,
     );
     var audio = http.MultipartFile.fromBytes(
       "audio",
@@ -20,14 +22,12 @@ class HttpSerivce {
     );
     print(request.toString());
     request.files.add(audio);
-    print(audio);
-    print(request.files.length);
+    // print(audio);
+    // print(request.files.length);
     var response = await request.send().timeout(const Duration(seconds: 15));
     print("Response: ${response.statusCode}");
     Uint8List responseData = await response.stream.toBytes();
     String result = String.fromCharCodes(responseData);
-    print("Result: $result");
+    return result;
   }
-
-  HttpSerivce();
 }
